a461a92517cf705e4a024d8fa4741c4f06c334ff
Sergey Kruchinkin <kruchinkin.s@yandex.ru>
2023-11-20 01:24:04 +0300

    add front

---
+++ front_react/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import axios from "axios";
+
+class App extends React.Component {
+  state = { details: [], }
+
+  componentDidMount() {
+    let data;
+    axios.get('http://localhost:8000')
+    .then(res => {
+      data = res.data;
+      this.setState({
+        details: data
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+  render() {
+    return (
+      <div>
+        <header>Данные из Django</header>
+        <hr></hr>
+        {this.state.details.map((output, id) =>(
+          <div key={id}>
+            <div>
+              <h2>{output.title}</h2>
+              <p>{output.channel}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default App;
