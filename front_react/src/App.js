b26f00088299079f07977f9d8558caa70f91d6d9
Sergey Kruchinkin <kruchinkin.s@yandex.ru>
2023-11-20 13:51:52 +0300

    lalalal

--- front_react/src/App.js
+++ front_react/src/App.js
@@ -26,8 +26,9 @@ class App extends React.Component {
         {this.state.details.map((output, id) => (
           <div key={id}>
             <div>
-              <h2>{output.title}</h2>
-              <p>{output.channel}</p>
+              <h2>{output.name}</h2>
+              <p>{output.last_name}</p>
+              <p>{output.age}</p>
             </div>
           </div>
         ))}
